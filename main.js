aa31acc8b34952c1b59e8e492bbbe1837bae3b50
yarangamunozmartes-a11y <yarangamunozmartes@gmail.com>
2025-12-10 01:52:15 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow, Notification, ipcMain } = require("electron");
+const path = require("path");
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 480,
+        height: 600,
+        resizable: false,
+        icon: path.join(__dirname, "assets/icon.png"), // 🦈 Ícono de la app
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+            contextIsolation: true,
+            nodeIntegration: false
+        }
+    });
+
+    win.loadFile("index.html");
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") app.quit();
+});
+
+// 🔔 Notificaciones desde renderer
+ipcMain.on("notificar", (e, datos) => {
+    new Notification({
+        title: datos.titulo,
+        body: datos.cuerpo,
+        icon: path.join(__dirname, "assets/icon.png") // 🦈 Ícono también en notificaciones
+    }).show();
+});
+
