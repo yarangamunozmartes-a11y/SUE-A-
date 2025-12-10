7d7969635ae7cdc6673eebe2111245437aa46f9b
yarangamunozmartes-a11y <yarangamunozmartes@gmail.com>
2025-12-10 00:22:57 -0500

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,8 +1,2 @@
-const { contextBridge, ipcRenderer } = require("electron");
 
-contextBridge.exposeInMainWorld("electronAPI", {
-    enviarNotificacion: (titulo, cuerpo) => {
-        ipcRenderer.send("notificar", { titulo, cuerpo });
-    }
-});
 
